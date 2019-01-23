d5f5e115e2e7ace441041f05ac1e02ae59045f44
358266 <34713033+358266@users.noreply.github.com>
2019-01-23 13:45:32 +0100

    Add files via upload
    Edit: Modulair.

--- js/index.js
+++ js/index.js
@@ -1,33 +1,34 @@
-jQuery(document).ready(function( $ ){
- function alerts (alert1, alert2, alert3, alert4) { 	//function calling all alerts
-		 var hours = new Date().getHours(); 	//get time by hours
 
-	 if (alert1 == undefined) {			// if statement that fills the alert if undefined 
-		 alert1=0;
-	 }
 
-	 if (alert2 == undefined) {
-		 alert2=12;
-	 }
+function functietje() {
+    this.alerts = function (alert1, alert2, alert3, alert4) { 	//function calling all alerts
+    		 var hours = new Date().getHours(); 	//get time by hours
 
-	 if (alert3 == undefined) {
-		 alert3=17;
-	 }
+    	 if (alert1 == undefined) {			// if statement that fills the alert if undefined
+    		 alert1=0;
+    	 }
 
-	 if (alert4 == undefined) {
-		 alert4=24;
-	 }
+    	 if (alert2 == undefined) {
+    		 alert2=12;
+    	 }
 
- if (hours >= alert1 && hours < alert2) {		//check if the time is between alert1 and alert 2, if than so execute
-		 document.body.style.backgroundColor = "blue";
- } else if (hours >= alert2 && hours < alert3) {
-				 document.body.style.backgroundColor = "red";
- } else if (hours >= alert3 && hours < alert4) {
-				 document.body.style.backgroundColor = "green";
- } else {
+    	 if (alert3 == undefined) {
+    		 alert3=17;
+    	 }
 
- }
+    	 if (alert4 == undefined) {
+    		 alert4=24;
+    	 }
 
- }
- alerts(a, b, c, d); 
- }); 							//end Jquery 
+     if (hours >= alert1 && hours < alert2) {		//check if the time is between alert1 and alert 2, if than so execute
+    		 document.body.style.backgroundColor = "blue";
+     } else if (hours >= alert2 && hours < alert3) {
+    				 document.body.style.backgroundColor = "red";
+     } else if (hours >= alert3 && hours < alert4) {
+    				 document.body.style.backgroundColor = "green";
+     } else {
+
+     }
+
+     }
+}
